2a1a3aadc8b004cbb3d2698042b0ae2c295bb175
Jody Klymak <jklymak@gmail.com>
2018-04-21 16:40:02 +0000

    Better Forcing

---
+++ python/getLevel.py
@@ -0,0 +1,9 @@
+import xarray as xr
+
+for td in ['IWNoLeith', 'IWAllLeith']:
+    todo = '../results/{}/input/final.nc'.format(td)
+    k = 50
+    with xr.open_dataset(todo) as ds:
+        print(ds)
+        ds = ds.isel(k=k, k_u=k, k_l=k, record=-1)
+        ds.to_netcdf('../reduceddata/{}/Level{:04d}.nc'.format(td, k))
--- python/getSlice.py
+++ python/getSlice.py
@@ -1,10 +1,9 @@
 import xarray as xr
 
-for f in ['038', '073', '100', '126', '141']:
-    td = 'LW1kmlowU10Amp305f' + f
+for td in [ 'IWAllLeith', 'IWNoLeith']:
     todo = '../results/{}/input/final.nc'.format(td)
 
     with xr.open_dataset(todo) as ds:
         print(ds)
-        ds = ds.isel(j=10, j_g=10, record=-1)
-        ds.to_netcdf('../reduceddata/AllSlice{}.nc'.format(td))
+        ds = ds.isel(j=128, j_g=128, record=-4)
+        ds.to_netcdf('../reduceddata/{}/AllSlice.nc'.format(td))
